fd427f169aac7bb182ef4e901181cd4e1694605f
MammadProMax <mhaagrm2@gmail.com>
2024-10-22 12:17:15 +0330

    animations

--- src/index.js
+++ src/index.js
@@ -1,91 +1,79 @@
 import * as Three from "three";
-
+import gsap from "gsap";
 // scene
-
 const scene = new Three.Scene();
-const canvas = document.getElementsByClassName("webgl")[0];
 
-// group
-const group = new Three.Group();
-scene.add(group);
-
-const cube1 = new Three.Mesh(
-   new Three.BoxGeometry(1, 1, 1),
-   new Three.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube1.position.x = 1.5;
-group.add(cube1);
-
-const cube2 = new Three.Mesh(
-   new Three.BoxGeometry(1, 1, 1),
-   new Three.MeshBasicMaterial({ color: 0x00ff00 })
-);
-cube2.position.x = 3;
-group.add(cube2);
-
-const cube3 = new Three.Mesh(
-   new Three.BoxGeometry(1, 1, 1),
-   new Three.MeshBasicMaterial({ color: 0x0000ff })
-);
-group.position.x = 3;
-group.add(cube3);
-
-// geometry and material
-
-const geometry = new Three.BoxGeometry(1, 1, 1);
-const material = new Three.MeshBasicMaterial({ color: 0xff0000 });
-
-// object
-
-const mesh = new Three.Mesh(geometry, material);
-// mesh.position.z = 0.5;
-// mesh.position.y = -1;
-// mesh.position.x = -2.5;
-
-mesh.position.set(-1, -1, -4);
-
-// scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 2);
-
-// rotation
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI / 2;
-mesh.rotation.y = Math.PI * 0.3;
-
-scene.add(mesh);
-
-// axes helper
-
-const axesHelper = new Three.AxesHelper(2);
-scene.add(axesHelper);
-
-// sizes
 const sizes = {
    width: 800,
    height: 600,
 };
 
-// camera : perspective
-
+// camera
 const camera = new Three.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 4;
-camera.position.x = group.position.x + cube1.position.x;
-camera.position.y = group.position.y;
-
+camera.position.z = 3;
 scene.add(camera);
 
-// to look at any object
-// camera.lookAt(mesh.position);
-// camera.lookAt(group.position);
-// console.log(camera.position.distanceTo(mesh.position));
+// mesh
+const geometry = new Three.BoxGeometry(1, 1, 1);
+const material = new Three.MeshBasicMaterial({ color: "#78f1ff" });
 
-const renderer = new Three.WebGLRenderer({
-   canvas,
-});
+const mesh = new Three.Mesh(geometry, material);
+scene.add(mesh);
 
+// render
+
+const renderer = new Three.WebGLRenderer();
 renderer.setSize(sizes.width, sizes.height);
+document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+// animation fps control with delta time
+
+// let time = Date.now();
+
+// const animate = () => {
+//    const currentTime = Date.now();
+//    const delta = currentTime - time;
+//    time = currentTime;
+
+//    mesh.rotation.y += 0.001 * delta;
+
+//    // render
+//    renderer.render(scene, camera);
+//    window.requestAnimationFrame(animate);
+// };
+// animate();
+
+// animation fps control with Threejs clock
+// const clock = new Three.Clock();
+// const animate = () => {
+//    const elapsedTime = clock.getElapsedTime();
+
+//    // mesh.rotation.y = elapsedTime * Math.PI * 2;
+//    camera.position.y = Math.sin(elapsedTime);
+//    camera.position.x = Math.cos(elapsedTime);
+//    camera.lookAt(mesh.position);
+//    // render
+//    renderer.render(scene, camera);
+//    window.requestAnimationFrame(animate);
+// };
+// animate();
+
+// animation with gsap
+// gsap.to(mesh.position, { x: 2, delay: 1, duration: 2 });
+// gsap.to(mesh.position, { x: -2, delay: 2, duration: 2 });
+
+// const animate = () => {
+//    // render
+//    renderer.render(scene, camera);
+//    window.requestAnimationFrame(animate);
+// };
+// animate();
+
+// animation with threejs api
+
+const animate = () => {
+   mesh.rotation.y += 0.01;
+   renderer.render(scene, camera);
+};
+
+renderer.setAnimationLoop(animate);
